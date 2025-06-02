93c0bb52f7a5155160f48084cfcc64b6ce5c3292
jamunesh15 <jsheta15@gmail.com>
2025-06-02 14:09:51 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Navbar from './Pages/Navbar'
 
 export const AuthContext = createContext()
 
-const API_BASE_URL = 'http://localhost:8080'
+const API_BASE_URL = 'https://log-red-4-api1.onrender.com'
 
 function App() {
   const [user, setUser] = useState(null)
